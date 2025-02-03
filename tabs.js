1fe0e293071fa4169153c5e677c025f406eb083b
Jubibani <strawberryloli3@gmail.com>
2025-02-03 11:05:21 +0800

    style: add animation to tab transitions

--- tabs.js
+++ tabs.js
@@ -4,7 +4,8 @@ function openTab(evt, tabName) {
     // Hide all tab content
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].style.maxHeight = null; // Reset max-height
+        tabcontent[i].style.display = "none"; // Hide the content
     }
 
     // Remove the active class from all tabs
@@ -14,6 +15,8 @@ function openTab(evt, tabName) {
     }
 
     // Show the current tab and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    var currentTab = document.getElementById(tabName);
+    currentTab.style.display = "block"; // Show the content
+    currentTab.style.maxHeight = currentTab.scrollHeight + "px"; // Set max-height to the scroll height
+    evt.currentTarget.className += " active"; // Add active class to the clicked tab
 } 
